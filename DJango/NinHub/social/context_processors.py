872ef6d1911ab6166b86d7830f8361657a01f057
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-10 20:43:27 +0530

    Delete DJango/NinHub directory

--- DJango/NinHub/social/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import Notification
-
-def notification_count(request):
-    if request.user.is_authenticated:
-        count = Notification.objects.filter(user=request.user, is_read=False).count()
-        return {"unread_notif_count": count}
-    return {"unread_notif_count": 0}
