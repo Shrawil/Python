45b6e864b436d1589345ee8cf77922f27bfb4528
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-16 21:51:14 +0530

    Refactor gamble.py for improved functionality
    Refactor gamble.py to improve readability and fix errors.

--- gamble.py
+++ gamble.py
@@ -1,46 +1,69 @@
 import random
 
-def runGamble(int: bal, int: diff):
+def runGamble(bal, diff):
+    cur = bal
     while True:
         try:
             betAmount = int(input("Enter an amount to bet : "))
             if betAmount > 0 and betAmount <= bal:
                 break
             else:
-                print("Bet amount should be greater than 0 and and smaller than or equal to your current ballance!")
+                print("Bet amount should be greater than 0 and smaller than or equal to your current ballance!")
+        except TypeError:
+            print("Please enter a valid integer!")
+    num = random.randint(1, 101)
     if diff == 1:
-        if randInt(1, 50) < 50:
-            bal 
+        if num <= 50: bal += betAmount
+        else: bal -= betAmount
+    elif diff == 2:
+        if num <= 30: bal += betAmount 
+        else: bal -= betAmount
+    elif diff == 3:
+        if num <= 10: bal += betAmount 
+        else: bal -= betAmount
+    elif diff == 4:
+        if num == 1: bal += betAmount
+        else: bal -= betAmount
+    if cur > bal:
+        print(f"You lost! -{betAmount}")
+    else:
+        print(f"You won! +{cur}")
     
 
-def gamble(int: balance):
+def gamble(balance):
     print("Choose difficulty : ")
     print("[1] Easy (Probability : 50% | x2)")
     print("[2] Medium (Probability : 30% | x3)")
     print("[3] Hard (Probability : 10% | x5)")
     print(f"[4] Feeling lucky (Probability : 1% | x10)")
-    choice : int
-    
     # To make sure user only enters an integer
     while True:
         try:
-            choice = int(input(">"))
-            break
+            difficulty = int(input(">"))
+            if difficulty > 4 or difficulty < 1:
+                print("Please choose between 1-4!")
+            elif difficulty <= 0:
+                print("You don't have enough money to enter!")
+                break
+            else:
+                runGamble(balance, difficulty)
+                break
         except TypeError:
             print("Please enter a valid integer.")
-    if choice <= 4 or choice >= 1 and balance > 0:
-        runGamble(balance, difficulty)
     
-def main(int: choice):
-    balance = 0
+def checkBalance(balance):
+    print(f"Current balance : {balance}.")
+
+def main(choice):
+    balance = 1000
     print("Welcome to gamble.py!")
     while True:
-        print("[1] Gamble | [2] Balance | [3] Exit")\
+        print("[1] Gamble | [2] Balance | [3] Exit")
         choice = int(input(">"))
         if choice == 1:
             gamble(balance)
         elif choice == 2:
-            checkBalance()
+            checkBalance(balance)
         elif choice == 3:
             break
         else: 
