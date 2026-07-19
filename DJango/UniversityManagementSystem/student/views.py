069c372b67ff4524846e7344b39433c534c2565c
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-19 20:30:42 +0530

    Finished University Django Project.

--- DJango/UniversityManagementSystem/student/views.py
+++ DJango/UniversityManagementSystem/student/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect 
+from .models import Students
+from .forms import StudentForms
 
 def show_students(request):
-    pass 
+    students = Students.objects.all()
+    return render(request, 'student/show_students.html', {'students':students})
 
 def add_students(request):
-    pass
+    if request.method == 'POST':
+        form = StudentForms(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('show_students')
+    else:
+        form = StudentForms()
+    return render(request, 'students/add_students.html', {'forms':form})
