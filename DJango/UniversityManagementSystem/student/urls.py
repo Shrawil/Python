23730b3d8b1ba52adba18c5b858b02f885afd1dc
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-19 19:40:42 +0530

    Added urls and views for student app.

---
+++ DJango/UniversityManagementSystem/student/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.show_students, name='show_students'),
+    path('add_students', views.add_students, name='add_students'),
+]
--- DJango/UniversityManagementSystem/student/views.py
+++ DJango/UniversityManagementSystem/student/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 
-# Create your views here.
+def show_students(request):
+    pass 
+
+def add_students(request):
+    pass
