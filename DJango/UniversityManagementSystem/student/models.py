8aa7ffb6f8c5bf0f0dea7b2c0a9c6b09a26214fa
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-19 20:08:46 +0530

    Added student model.

---
+++ DJango/UniversityManagementSystem/student/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from department.models import Department
+
+CHOICE = [1,2,3]
+
+class Students(models.Model):
+    student_name = models.CharField(max_length=30)
+    student_age = models.IntegerField()
+    student_email = models.EmailField()
+    student_year = models.IntegerField(choices=CHOICE)
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.CASCADE
+    )
+    def __str__(self):
+        year = self.student_year
+        if year == 1: 
+            yr = 'st'
+        elif year == 2:
+            yr = 'nd'
+        elif year = 3:
+            yr = 'rd'
+        return f"{self.student_name} | {self.department.dep_name} {year}{yr} Year"
