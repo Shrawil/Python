d45e1cb3de062337ec6197905782f99fe411bd58
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-21 19:39:31 +0530

    Updated forLoop.py program.

--- forLoop.py
+++ forLoop.py
@@ -16,39 +16,50 @@ for i in range(0, 10, 2):
     print(i, end="")
 print("\n")
 
-print("# Reverse counting ")
+print("# Reverse counting.")
 print("for i in range(11, 1, -1):\n\tprint(i, end=\"\")")
 for i in range(9, -1, -1): 
     print(i, end="")
 print("\n")
 
-print("# Reverse counting with gaps")
+print("# Reverse counting with gaps.")
 print("for i in range(20, 0, -2):\n\tprint(i, end=\"\")")
 for i in range(9, -1, -2):
     print(i, end="")
 print("\n")
 
-print("# Print letters")
+print("# Print letters.")
 WORD = "HELLO WORLD"
 print("WORD = \"HELLO WORLD\"\nfor i in WORD:\n\tprint(i, end=\"\")")
 for i in WORD:
     print(i, end="")
 print("\n")    
 
-print("# Print reversed letter")
+print("# Print reversed letter.")
 print("WORD = \"HELLO WORLD\"\nfor i in range(len(word)-1, -1, -1):\n\tprint(WORD[i], end=\"\")")
 for i in range(len(WORD)-1, -1, -1):
     print(WORD[i], end="")
 print("\n")
 
-print("# Printing lists")
+print("# Printing lists.")
 NUMBERS = [1,3,2,5]
 print("NUMBERS = [1,3,2,5]\nfor i in NUMBER:\n\tprint(i, end=\"\")")
 for i in NUMBERS:
     print(i, end="")
 print("\n")
 
-print("# For both index and values")
+print("# For both index and values.")
 print("NUMBERS = [1,3,2,5]\nfor i in range(len(NUMBERS)):\n\tprint(f\"NUMBERS[{i}] = {NUMBERS[i]}\")")
 for i in range(len(NUMBERS)):
     print(f"NUMBERS[{i}] = {NUMBERS[i]}")
+
+print("# Ignore loop variable. (When you don't care about loop variable.)")
+print("for _ in range(5):\n\tprint(\"Hello world!\")")
+for _ in range(5):
+    print("Hello world!")
+
+print("# Nested loop.")
+print("for i in range(2):\n\tfor j in range(2):\n\t\tprint(i, j)")
+for i in range(2):
+    for j in range(2):
+        print(i, j)
