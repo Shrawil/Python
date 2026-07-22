8cd4d1923251c386acd6a465e1fb5342986cdd97
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-22 19:24:59 +0530

    Created a main.py for training related programs and practice.

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+"""
+start = int(input("Enter value for start : "))
+end = int(input("Enter value for end : "))
+step = int(input("Enter value for step : "))
+
+while start <= end:
+    print(start, end=" ")
+    start += step
+"""
+
+"""
+a = int(input("Enter a number : "))
+
+if a % 3 == 0: 
+    print('FIZZ', end="")
+if a % 5 == 0:
+    print('BUZZ', end="")
+"""
