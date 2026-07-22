3cf02845c35682f8d30ba380625046675c90dad9
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-22 19:32:07 +0530

    Added logic for get_and_check() function.

--- attendance.py
+++ attendance.py
@@ -1,5 +1,14 @@
-def get_and_check(msg: str):
-    pass 
+def get_and_check(msg: str, *args):
+    while True:
+        try:
+            choice = int(input(msg))
+            if choice in args:
+                return choice 
+            else:
+                print(f"Please choose between {args[0]} - {args[len(args)-1]}.")
+        except ValueError:
+            print("Please enter a valid value.")
+
 
 def take_attendance():
     absent = 0
@@ -8,7 +17,7 @@ def take_attendance():
 
 def main():
     while True:
-        choice = get_and_check('[1] Take Attendance | [2] Exit > ')
+        choice = get_and_check('[1] Take Attendance | [2] Exit > ', 1, 2)
         if choice == 1:
             present, absent = take_attendance()
         elif choice == 2:
