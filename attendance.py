e974b2806a67ab883031bed896f358c2bc71a6e9
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-22 19:37:53 +0530

    Minor fix in main and get_and_check function.

--- attendance.py
+++ attendance.py
@@ -1,11 +1,8 @@
-def get_and_check(msg: str, *args):
+def get_and_check(msg: str):
     while True:
         try:
             choice = int(input(msg))
-            if choice in args:
-                return choice 
-            else:
-                print(f"Please choose between {args[0]} - {args[len(args)-1]}.")
+            return choice 
         except ValueError:
             print("Please enter a valid value.")
 
@@ -13,17 +10,21 @@ def get_and_check(msg: str, *args):
 def take_attendance():
     absent = 0
     present = 0
+    total_students = get_and_check('Enter number of students : ')
     return present, absent
 
 def main():
     while True:
-        choice = get_and_check('[1] Take Attendance | [2] Exit > ', 1, 2)
+        choice = get_and_check('[1] Take Attendance | [2] Exit > ')
         if choice == 1:
             present, absent = take_attendance()
             print(f"Total number of presentees : {present}.\nTotal number of absentees : {absent}.")
         elif choice == 2:
             print("Thanks for using this program!")
             break
+        else:
+            print("Please choose between 1 and 2.")
+            choice = get_and_check('[1] Take Attendance | [2] Exit > ')
     
 
 if __name__ == '__main__':
