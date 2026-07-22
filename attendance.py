8c1838fc65560aa40411f865ca3b066bcc8f692b
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-22 19:43:47 +0530

    Script complete, bug fixing left.

--- attendance.py
+++ attendance.py
@@ -8,9 +8,18 @@ def get_and_check(msg: str):
 
 
 def take_attendance():
-    absent = 0
-    present = 0
+    # Will hold all roll numbers
+    present = list()
+    absent = list()
+    
     total_students = get_and_check('Enter number of students : ')
+    print("Type 1 if present. [Note: Any other number than 1 will mark the roll number as absent.]")
+
+    for i in range(total_students):
+        is_present = get_and_check(f'Roll number {i+1} : ')
+        if is_present == 1: present.append(i+1)
+        else: absent.append(i+1)
+
     return present, absent
 
 def main():
