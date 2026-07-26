105a3b2ec7a57b1c99491896228d365e7a669dd6
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-26 17:48:46 +0530

    Added more functionality in RotateList.py (User can now enter number of time a list should be rotated.

--- PACS-Tasks/Task3/RotateList.py
+++ PACS-Tasks/Task3/RotateList.py
@@ -7,19 +7,21 @@
 # 2. ls[i] = ls[i+1]
 # 3. Last element = temp
 
-def rotate(ls):
-    temp = ls[0]
-    for i in range(len(ls)):
-        if i == len(ls)-1:
-            ls[i] = temp
-        else:
-            ls[i] = ls[i+1]
+def rotate(ls, t):
+    for time in range(t):
+        temp = ls[0]  
+        for i in range(len(ls)-1):
+           ls[i] = ls[i+1]
+        ls[-1] = temp
+        
     return ls 
 
 def main():
     ls = [1,2,3,4,5,6,7]
     print(ls)
-    print(rotate(ls))
+    # Pass the list and the times you want to rotate the list.
+    t = int(input("Enter number of times you want to rotate the list : "))
+    print(rotate(ls,t))
     '''
     for i in range(1000):
         print(rotate(ls), end="\r", flush=True)
