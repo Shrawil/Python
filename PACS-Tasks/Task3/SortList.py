6233e3a9d3abf23310d659f2fb4bbbdf339135d9
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-26 13:47:39 +0530

    Sorting a list in ascending and descending.

---
+++ PACS-Tasks/Task3/SortList.py
@@ -0,0 +1,12 @@
+# Sort a list in ascending order.
+# Sort a list in descending order.
+
+ls = [1,2,6,5,2,9,7,0]
+
+print(ls)
+
+ls = sorted(ls)
+print(ls)
+
+ls = sorted(ls, reverse=True)
+print(ls)
