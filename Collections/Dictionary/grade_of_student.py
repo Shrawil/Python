bf1c137d22cf1600cf8a65368e69cfbcccac7bba
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-28 22:13:05 +0530

    Created a program to store and print student's name and grade using dictionary.

---
+++ Collections/Dictionary/grade_of_student.py
@@ -0,0 +1,19 @@
+from myLib.easyInput import eval_list 
+
+def get_grades(names: list):
+    res = {}
+    for name in names:
+        grade = input(f"Enter grade for {name} [A,B,C,D,E,F] : ")
+        res[name] = grade
+    return res
+
+
+def main():
+    names = eval_list("Enter name of students : ")
+    grade_dict = get_grades(names)
+
+    for item in grade_dict:
+        print(f'{item} : {grade_dict.get(item)}')
+
+if __name__ == '__main__':
+    main()
