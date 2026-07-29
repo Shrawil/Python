f030fb07fed2e13b28a87fbbd571a0e53999233b
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-29 21:23:00 +0530

    Create file with basic methods of os module in python.

--- Libraries/datetime_module.py
+++ Libraries/datetime_module.py
@@ -1,12 +1,20 @@
 import datetime, time
 
+today = datetime.datetime.now()
+
+print((round(float(today.microsecond)) * (round(float(today.second)) ** round(float(today.microsecond)))) % 10)
+
 '''
 print(today)
 
 print(today.time())
 print(today.date())
-print(today.weekday())
+print(today.weekday()) # 0 Monday 1 Tuesday 2 Wednesday 3 Thursday 4 Friday 5 Saturday 6 Sunday
 
+print(today.hour)
+print(today.minute)
+print(today.second)
+print(today.microsecond)
 print(today.day)
 print(today.month)
 print(today.year)
---
+++ Libraries/os_module.py
@@ -0,0 +1,6 @@
+import os
+
+print(os.getcwd())
+print(os.cpu_count())
+print(os.mkdir('test'))
+print(os.removedirs('test'))
