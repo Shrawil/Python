a18347e5c1ab283c37ae40c98e0400d93fbc1439
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-07-29 21:45:08 +0530

    Created a program with basic methods and functions of sys module.

---
+++ Libraries/sys_module.py
@@ -0,0 +1,9 @@
+import sys
+
+print(sys.version)
+print(sys.platform)
+print(sys.base_prefix)
+print(sys.getrecursionlimit())
+print(sys.getallocatedblocks())
+print(sys.getdefaultencoding())
+print(sys.getwindowsversion())
