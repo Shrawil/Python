32649e0e3b567aabda43cb8bfc1135d9d3d18e57
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-08-20 23:26:33 +0530

    Learning numpy, getting started.

---
+++ Libraries/numpy/first.py
@@ -0,0 +1,34 @@
+import numpy
+
+arr = numpy.array(1) # 0 Dimension array.
+
+# Print items of array.
+print(arr)
+
+# Prints the type of array.
+print(type(arr))
+
+# Prints the dimention of array.
+print(arr.ndim)
+
+arr = numpy.array([1,2,3,4]) # 1 Dimension array.
+
+# Print items of array.
+print(arr)
+
+# Prints the type of array.
+print(type(arr))
+
+# Prints the dimention of array.
+print(arr.ndim)
+
+arr = numpy.array([[1,2,3],[4,5,6]]) # 2 Dimension array.
+
+# Print items of array.
+print(arr)
+
+# Prints the type of array.
+print(type(arr))
+
+# Prints the dimention of array.
+print(arr.ndim)
