5823dbf17e0a6b2c006767cc59bb9f5e70a55f96
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-08-21 23:18:55 +0530

    Reshaping an array in numpy.

--- Libraries/numpy/shape.py
+++ Libraries/numpy/shape.py
@@ -12,3 +12,11 @@ arr = np.array([1, 2, 3, 4], ndmin=5)
 
 print(arr)
 print('shape of array :', arr.shape)
+
+# Reshaping 
+# By reshaping we can add or remove dimensions or change number of elements in each dimension.
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(4, 3) # 4 rows of 3 element each, so the original array must have 4*3 elements.
+
+print(newarr)
