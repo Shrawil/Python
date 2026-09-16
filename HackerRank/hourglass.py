ea1e133c3965f532b0e0ffd4f07c8c327442da03
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-09-17 00:01:57 +0530

    Hourglass for 2D array.

--- HackerRank/hourglass.py
+++ HackerRank/hourglass.py
@@ -1,26 +1,15 @@
 def hourglassSum(arr):
     # Write your code here
-    print(arr)
-    row = 0
-    sum = arr[0]
-    highest = sum
-    while row < 24:
-        for i in range(row, row+4):
-            try:
-                print(f"{arr[i]}\t{arr[i+1]}\t{arr[i+2]}\n\t{arr[i+7]}\n{arr[i+12]}\t{arr[i+13]}\t{arr[i+14]}")
-            except:
-                print("Program stopped at index : ", i)
-            sum = arr[i] + arr[i+1] + arr[i+2] + arr[i+7] + arr[i+12] + arr[i+13] + arr[i+14]
-        if highest < sum:
-            highest = sum
-        row += 6
+    sum = 0
+    highest = -float('inf')
+    for r in range(4):
+        for c in range(len(arr)-2):
+            #print(f"{arr[r][c]}\t{arr[r][c+1]}\t{arr[r][c+2]}\n\t{arr[r+1][c+1]}\n{arr[r+2][c]}\t{arr[r+2][c+1]}\t{arr[r+2][c+2]}")
+            sum = arr[r][c] + arr[r][c+1] + arr[r][c+2] + arr[r+1][c+1] + arr[r+2][c] + arr[r+2][c+1] + arr[r+2][c+2]
+            if highest < sum:
+                highest = sum
     return highest
 
-print(hourglassSum([
-    1,1,1,0,0,0,
-    0,1,0,0,0,0,
-    1,1,1,0,0,0,
-    0,0,0,0,0,0,
-    0,0,0,0,0,0,
-    0,0,0,0,0,0,
-]))
+print(hourglassSum(
+    [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 2, 4, 4, 0], [0, 0, 0, 2, 0, 0], [0, 0, 1, 2, 4, 0]]
+))
